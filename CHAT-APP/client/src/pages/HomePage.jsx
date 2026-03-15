fbe9480ebe7578aaafe4e7b561d0da193c40d910
Rishabh Bajpai <rishabhbajpai131@gmail.com>
2026-03-15 18:15:34 +0530

    resource collected

---
+++ CHAT-APP/client/src/pages/HomePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <div className='border w-full h-screen'>
+      <h1>Home Page</h1>
+    </div>
+  )
+}
+
+export default HomePage
